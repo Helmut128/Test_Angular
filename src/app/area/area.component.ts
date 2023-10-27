a2f1933e089d854deb32e5f988fc45091aa04197
hdr_inventory_20230609 <helmutmartinez128@gmail.com>
2023-10-27 08:14:57 -0600

    Implementando cambios para editar

--- src/app/area/area.component.ts
+++ src/app/area/area.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-area',
@@ -17,6 +18,37 @@ export class AreaComponent {
       // this.users = users;
       this.dataSource = data;
       console.log(data);
+    });
+  }
+
+  onRowUpdating(event: any) {
+    const updatedData = event; // Los datos editados
+    console.log(updatedData.idArea);
+
+    // Asegúrate de que updatedData contenga una propiedad 'id' válida
+    /*  if (updatedData && updatedData.id) {
+      this.apiService.updateData(updatedData).subscribe((response) => {
+        if (response.success) {
+          event.cancel = true; // Cancela la actualización localmente
+        } else {
+          // Maneja el error
+        }
+      });
+    } else {
+      console.error('ID no válido', updatedData);
+      // Maneja el caso en el que updatedData no tenga un 'id' válido
+    }*/
+  }
+
+  onRowRemoving(event: any) {
+    const removedData = event.data; // Datos del registro a eliminar
+    // Envía la solicitud de eliminación al servidor (a través de tu servicio ApiService)
+    this.apiService.deleteData(removedData.id).subscribe((response) => {
+      if (response.success) {
+        // El registro se eliminó con éxito
+      } else {
+        // Maneja el error
+      }
     });
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { environment } from '../enviroments/environments';
 
 @Injectable({
@@ -31,5 +31,25 @@ export class ApiService {
   //retorna el menu
   getMenu(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/menu`);
+  }
+
+  // Método para actualizar un registro en la base de datos
+  updateData(updatedData: any): Observable<any> {
+    if (updatedData && updatedData.id) {
+      return this.http.put<any>(
+        `${this.apiUrl}/updateData/${updatedData.id}`,
+        updatedData
+      );
+    } else {
+      // Manejar el caso en el que updatedData no tenga un ID válido.
+      console.error('ID no válido', updatedData);
+      return throwError('ID no válido');
+    }
+  }
+
+  // Método para eliminar un registro en la base de datos
+  deleteData(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/deleteData/${id}`);
+    // En este ejemplo, se asume que en el servidor tienes una ruta '/deleteData/:id' para eliminar un registro.
   }
 }
